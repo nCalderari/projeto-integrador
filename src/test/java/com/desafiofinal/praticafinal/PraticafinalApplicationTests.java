310e91fd0eae055fc213e7c7559aaf2a931fda23
amandaamabili <amanda.amabili@mercadolivre.com>
2022-08-05 11:58:06 -0300

    Created template PR,ISSUE and CI

--- src/test/java/com/desafiofinal/praticafinal/PraticafinalApplicationTests.java
+++ src/test/java/com/desafiofinal/praticafinal/PraticafinalApplicationTests.java
@@ -3,11 +3,11 @@ package com.desafiofinal.praticafinal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class PraticafinalApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+@SpringBootTest
+class PraticafinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
