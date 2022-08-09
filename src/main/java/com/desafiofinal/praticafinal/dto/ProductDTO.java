0eb504a637f5c1c2196c34393cbf6af7d6580121
amandaamabili <amanda.amabili@mercadolivre.com>
2022-08-09 15:54:59 -0300

    refact DTO

--- src/main/java/com/desafiofinal/praticafinal/dto/ProductDTO.java
+++ src/main/java/com/desafiofinal/praticafinal/dto/ProductDTO.java
@@ -14,48 +14,18 @@ public class ProductDTO {
     private String productName;
     private String productType;
     private double price;
-    private Seller seller;
+    private Long sellerId;
     private double bulk;
 
     public ProductDTO(Product product) {
         this.productName = product.getProductName();
         this.productType = product.getProductType();
         this.price = product.getPrice();
-        this.seller = product.getSeller();
         this.bulk = product.getBulk();
     }
 
-    public String getProductName() {
-        return productName;
+    public Product toModel() {
+        return new Product(this.productName, this.price, this.sellerId);
     }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public Seller getSeller() {
-        return seller;
-    }
-
-    public void setSeller(Seller seller) {
-        this.seller = seller;
-    }
-
-    public double getBulk() {
-        return bulk;
-    }
-
-    public void setBulk(double bulk) {
-        this.bulk = bulk;
-    }
-
 
 }
