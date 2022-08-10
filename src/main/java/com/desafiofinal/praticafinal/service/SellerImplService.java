3a9b0bc3b49ed6a69fd5d15585687d37c8f840c7
amandaamabili <amanda.amabili@mercadolivre.com>
2022-08-10 15:06:33 -0300

    Created try catch in seller service

--- src/main/java/com/desafiofinal/praticafinal/service/SellerImplService.java
+++ src/main/java/com/desafiofinal/praticafinal/service/SellerImplService.java
@@ -1,9 +1,12 @@
 package com.desafiofinal.praticafinal.service;
 
 import com.desafiofinal.praticafinal.dto.SellerDTO;
+import com.desafiofinal.praticafinal.exception.ExceptionDetails;
+import com.desafiofinal.praticafinal.handler.HandlerException;
 import com.desafiofinal.praticafinal.model.Seller;
 import com.desafiofinal.praticafinal.repository.ISellerRepo;
 import lombok.val;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -18,8 +21,13 @@ public class SellerImplService implements ISellerService{
 
     @Override
     public SellerDTO saveSeller(SellerDTO seller) {
-        val sellerSaved = repo.save(seller.toEntity());
-        var sellerConverted = new SellerDTO(sellerSaved);
-        return sellerConverted;
+        try{
+            val sellerSaved = repo.save(seller.toEntity());
+            var sellerConverted = new SellerDTO(sellerSaved);
+            return sellerConverted;
+        }
+       catch (Exception e){
+            throw  new Error("Desculpe, não foi possível realizar a sua solicitação", e.getCause());
+       }
     }
 }
