3d3c6207b3c64fd776992fe7d1d3216da478bbc3
amandaamabili <amanda.amabili@mercadolivre.com>
2022-08-10 15:09:19 -0300

    remove lines not used

--- src/main/java/com/desafiofinal/praticafinal/service/SellerImplService.java
+++ src/main/java/com/desafiofinal/praticafinal/service/SellerImplService.java
@@ -1,15 +1,10 @@
 package com.desafiofinal.praticafinal.service;
 
 import com.desafiofinal.praticafinal.dto.SellerDTO;
-import com.desafiofinal.praticafinal.exception.ExceptionDetails;
-import com.desafiofinal.praticafinal.handler.HandlerException;
-import com.desafiofinal.praticafinal.model.Seller;
 import com.desafiofinal.praticafinal.repository.ISellerRepo;
 import lombok.val;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 
 @Service
 public class SellerImplService implements ISellerService{
@@ -23,11 +18,10 @@ public class SellerImplService implements ISellerService{
     public SellerDTO saveSeller(SellerDTO seller) {
         try{
             val sellerSaved = repo.save(seller.toEntity());
-            var sellerConverted = new SellerDTO(sellerSaved);
-            return sellerConverted;
+            return new SellerDTO(sellerSaved);
         }
        catch (Exception e){
-            throw  new Error("Desculpe, não foi possível realizar a sua solicitação", e.getCause());
+            throw new Error("Desculpe, não foi possível realizar a sua solicitação", e.getCause());
        }
     }
 }
