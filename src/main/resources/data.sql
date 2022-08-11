e062a52cbd6493668030639f6b7bf9f06ceb20e9
amandaamabili <amanda.amabili@mercadolivre.com>
2022-08-11 11:38:30 -0300

    Created populate database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+show tables;
+-- INSERT INTO seller (id, seller_name) VALUES ("1","vendedor1");
+
+-- INSERT INTO manager  (manager_id, manager_name) VALUES ("1","1");
+
+-- INSERT INTO ware_house (ware_house_id, id_manager) VALUES ("1","1");
+
+-- INSERT INTO sector (sector_id, capacity, category, id_warehouse) VALUES ("1","1", "frios", "1");
+
+-- INSERT INTO product (id, bulk, price, product_id, product_name,product_type, id_seller ) VALUES ("1","1","10","um","banana", "fruta", "1");
