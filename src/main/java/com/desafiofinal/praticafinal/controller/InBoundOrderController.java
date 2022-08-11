02ddd5252034f0e02958dcf8eca9f46c9c3e2646
nCalderari <nicole.calderari@mercadolivre.com>
2022-08-11 11:53:41 -0300

    documentação do product controller

--- src/main/java/com/desafiofinal/praticafinal/controller/InBoundOrderController.java
+++ src/main/java/com/desafiofinal/praticafinal/controller/InBoundOrderController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
  * This class holds all endpoints related to inbound orders
  * @author Monica, Yago, Marina, Amanda
  * @version 1.0.1
- * @see Requirement 1 docs: encurtador.com.br/pC189
+ * @see Requirement 1 docs: https://br-playground.digitalhouse.com/course/86ba8e00-da33-420a-a62a-02d4a77c55e8/unit/f1c410da-fa91-44a9-b216-b6b93b85246c/lesson/39480008-71db-4c4a-af64-12766a2245a7/topic/21261b7d-ee61-46cc-b8e3-ab7dffec542f
  */
 
 @RestController
--- src/main/java/com/desafiofinal/praticafinal/controller/ProductController.java
+++ src/main/java/com/desafiofinal/praticafinal/controller/ProductController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * This class holds all endpoints related to the product
+ * @author Yago, Mônica, Marina
+ * @version 1.0.0
+ * @see Requirement 3 docs: https://br-playground.digitalhouse.com/course/86ba8e00-da33-420a-a62a-02d4a77c55e8/unit/f1c410da-fa91-44a9-b216-b6b93b85246c/lesson/6d242a1a-4961-4105-be9c-9189eb8eb6bc/topic/efd4119b-f679-48db-b77a-f7f18e0ca1c6
+ */
+
 @RestController
 @RequestMapping("/api/v1/fresh-products/product")
 public class ProductController {
@@ -19,23 +26,37 @@ public class ProductController {
         this.service = service;
     }
 
+    /**
+     * This route creates a list of products alongside the seller information
+     * @param product ProductName(String), ProductType(String), ValidateDate(LocalDate), Price(double), IdSeller(Long), Bulk(double).
+     * @return HTML Response 201: Created
+     */
     @PostMapping
     public ResponseEntity<ProductDTOWithSeller> insertProduct(@RequestBody ProductDTOWithSeller product){
             ProductDTOWithSeller response = service.saveProduct(product);
             return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    /**
+     * This route lists all products
+     * @return HTML Response 201: Created
+     */
     @GetMapping("/products")
     ResponseEntity<List<ProductDTO>> getAllProducts(){
 
-        return new ResponseEntity<List<ProductDTO>>(service.listAllProducts(),HttpStatus.FOUND);
+        return new ResponseEntity<List<ProductDTO>>(service.listAllProducts(),HttpStatus.OK);
 
     }
 
-
+    /**
+     * This route lists all products from a specific category
+     * @param category String containing the category name
+     * @return HTML Response 201: Created
+     * @throws Exception 
+     */
     @GetMapping("/{category}")
     ResponseEntity<List<ProductResponseDTO>> getProductBySector(@PathVariable String category)throws Exception{
-        return new ResponseEntity<List<ProductResponseDTO>>(service.listProductsByCategory(category),HttpStatus.FOUND);
+        return new ResponseEntity<List<ProductResponseDTO>>(service.listProductsByCategory(category),HttpStatus.OK);
 
     }
 }
