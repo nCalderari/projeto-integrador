865678ec3540eec3de5c6835a6d796f62393094f
nCalderari <nicole.calderari@mercadolivre.com>
2022-08-11 14:48:06 -0300

    removendo query do repo

--- src/main/java/com/desafiofinal/praticafinal/repository/IProductRepo.java
+++ src/main/java/com/desafiofinal/praticafinal/repository/IProductRepo.java
@@ -2,11 +2,9 @@ package com.desafiofinal.praticafinal.repository;
 
 import com.desafiofinal.praticafinal.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IProductRepo extends JpaRepository<Product, Long> {
-    @Query
-    public Product listProdutct();
+
 }
